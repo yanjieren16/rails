d8f43067504bf8a9a224f6ecc81d2e0e3d92c2d7
yanjieren <yanjie.ren16@gmail.com>
2018-12-06 11:41:29 -0800

    change the format for display

--- store/config/routes.rb
+++ store/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'shopper/index'
+  get 'shopper', to:'shopper#index'
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "shopper#index", as: 'root'
